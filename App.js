20a887a7c0bbde4d8be93676c8e899d3d5a1842c
thedannysoto <87dsoto@gmail.com>
2020-06-29 19:04:07 -0500

    Creates Header.js component and adds styles to Header and imports into App.js

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <View>
-
+    <View style={styles.screen}>
+      <Header titile="Guess A Number" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+    screen: {
+      flex: 1
+    }
 });
