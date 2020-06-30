734624681e9cffe5d329c917e533893fef354d87
thedannysoto <87dsoto@gmail.com>
2020-06-30 11:40:18 -0500

    Creates Input component to handle user input, adds styling to Input component, import TouchableWithoutFeedback to StartGameScreen to dismiss on screen keyboard with a tap

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,16 +1,42 @@
-import React from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    Button, 
+    TouchableWithoutFeedback, 
+    Keyboard } from 'react-native';
 
 import Card from '../components/Card';
 import Colors from '../constants/colors';
+import Input from '../components/Input';
 
 const StartGameScreen = props => {
+
+    const [enteredValue, setEnteredValue] = useState('');
+
+    const numberInputHandler = inputText => {
+        setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+    };
+
     return (
+        <TouchableWithoutFeedback onPress={() => { 
+            Keyboard.dismiss();
+         }}>
         <View style={styles.screen}>
             <Text style={styles.title}>Start a New Game!</Text>
             <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
-                <TextInput />
+                <Input 
+                    style={styles.input} 
+                    blurOnSubmit 
+                    autoCapitalize='none' 
+                    autoCorrect={false} 
+                    keyboardType="number-pad" 
+                    maxLength={2} 
+                    onChangeText={numberInputHandler}
+                    value={enteredValue}
+                />
                 <View style={styles.buttonContainer}>
                     <View style={styles.button}>
                         <Button title="Reset" onPress={() => {}} color={Colors.accent} />
@@ -21,6 +47,7 @@ const StartGameScreen = props => {
                 </View>
             </Card>
         </View>
+        </TouchableWithoutFeedback>
     );
 };
 
@@ -47,6 +74,10 @@ const styles = StyleSheet.create({
     },
     button: {
         width: 100
+    },
+    input: {
+        width: 50,
+        textAlign: 'center'
     }
 });
 
